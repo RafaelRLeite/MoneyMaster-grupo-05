79c48e2b6e23dba3500198434459a66879ca3cde
Douglas Barbosa <douglas.barbosa@sensedia.com>
2023-03-09 21:38:22 -0300

    [MM-1] Criando estrutura de pastas

---
+++ src/main/java/com/javeiros/brasil/moneymaster/MoneyMasterApplication.java
@@ -0,0 +1,13 @@
+package com.javeiros.brasil.moneymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javeiros/brasil/moneymaster/MoneyMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javeiros.brasil.moneymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
